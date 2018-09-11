29130f6ab18e6c71efce4dcabccc4b4b07c51458
Jyothi C <jyothi1391@gmail.com>
2018-09-11 17:33:09 +0530

    Delete test_01_login.py

--- Amz_POM/testScripts/test_01_login.py
+++
@@ -1,9 +0,0 @@
-from Amz_POM.common.basetest import BaseTestClass
-from Amz_POM.pageObject.page_actions import PageActionsClass
-
-class test_01_login(BaseTestClass):
-
-    pg_act = PageActionsClass()
-    PageActionsClass.login_action(pg_act)
-
-
